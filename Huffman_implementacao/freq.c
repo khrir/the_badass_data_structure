e65debc67459556bc4b5b5bbe17f6bc7ca0447b3
Gustavo Alexadre Silva <alexandregustavo00@gmail.com>
2024-02-29 22:44:57 -0300

    errinho no typedef do ulli

--- Huffman_implementacao/freq.c
+++ Huffman_implementacao/freq.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 typedef unsigned char uchar;
-typedef ulli ulli;
+typedef unsigned long long int ulli;
 
 /*Função que cria um vetor de frequência, o calloc foi utlizado para inicializar o vetor com 0
 sem a necessidade de um for para inicializar o vetor nem a necessidade da chamada da funão memset*/
