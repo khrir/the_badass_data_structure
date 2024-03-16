e4cba508df3c09e369f888c2d2608acd2b323509
Gustavo Alexadre Silva <alexandregustavo00@gmail.com>
2024-03-16 20:23:59 -0300

    retirando puts de teste

--- Huffman_implementacao/compress/compress.c
+++ Huffman_implementacao/compress/compress.c
@@ -10,8 +10,6 @@ void comprimir(char *nome_arquivo){
     criar_huff_tree(fila);
     ulli tamanho_arvore = 0;
     tamanho_huff_tree(fila->head, &tamanho_arvore);
-    puts("");
-    puts("Tamanho da arvore calculado!");
     escrv_bytes_cod(nome_arquivo, fila->head);
     agrupar_em_bytes_e_salvar("caminho_bytes.txt");
     salvar_no_arquivo(nome_arquivo, tamanho_arvore, fila);
